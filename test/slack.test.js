627828b4c3fb781f3db2561e30ca7fb0484483ed
Chris <orthagonal1@gmail.com>
2016-08-06 11:10:45 -0500

    plugin registering
    adds mocha testing also

--- test/slack.test.js
+++ test/slack.test.js
@@ -1,31 +1,34 @@
 'use strict';
 const expect = require('chai').expect;
-const Logr = require('../');
+// this is a logr plugin, so it should be
+// installed as a package inside logr using
+// 'npm install logr-slack':
+const Logr = require('../../../index.js');
 const path = require('path');
 const Hapi = require('hapi');
 
 it('can load the slack plugin ', (done) => {
   const server = new Hapi.Server({});
   const log = new Logr({
-    type: 'slack',
+    type: 'logr-slack',
     plugins: {
-      slack: 'logr-slack'
+      'logr-slack': 'logr-slack'
     },
     renderOptions: {
-      slack: {
+      'logr-slack': {
         server,
+        moduleName: 'logr-slack',
         slackHook: process.env.SLACK_WEBHOOK,
         // you can provide a name for any channel allowed by the above slack webhook:
         channel: '#hapi-slack-test',
         // you can list which tags will cause a server.log call to post to slack:
         tags: ['warning', 'error', 'success', 'test'],
         // you can specify tags that will automatically be appended to each post to slack:
-        additionalTags: ['server-test.js', 'someAdditionalTag'],
-
+        additionalTags: ['server-test.js', 'someAdditionalTag']
       }
     }
   });
-  log('there');
   expect(server.methods.postMessageToSlack).to.not.equal(undefined);
+  expect(server.methods.postRawDataToSlack).to.not.equal(undefined);
   done();
 });
