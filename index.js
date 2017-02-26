ec194af49644d397fd60a96b6743bd437c5f6b53
Chris <orthagonal1@gmail.com>
2017-02-26 17:01:49 -0600

    v1

--- index.js
+++ index.js
@@ -1,113 +1,17 @@
 'use strict';
-const _ = require('lodash');
-const Wreck = require('wreck');
+const Post2Slack = require('post2slack');
 
-let options = {};
-
-// used by slackPostMessage to construct a nice payload:
-const makeSlackPayload = (tags, data) => {
-  //clone because we muck with data
-  data = _.cloneDeep(data);
-  const attachment = {
-    fields: []
-  };
-
-  if (_.isString(data)) { //if string just pass as title and be done with it
-    attachment.title = data;
-    attachment.fallback = data;
-  } else if (_.isObject(data)) { // if object, then lets make it look good
-    //if it has a message, pull that out and display as title
-    if (data.message) {
-      attachment.title = data.message;
-      attachment.fallback = data.message;
-      delete data.message;
-    }
-    if (data.url) {
-      attachment.title_link = data.url;
-      delete data.url;
-    }
-    attachment.text = `\`\`\` ${JSON.stringify(data, null, '  ')} \`\`\``;
-    attachment.mrkdwn_in = ['text'];
+module.exports.log = function(options, tags, message) {
+  if (!message) {
+    message = tags;
+    tags = options;
+    options = false;
   }
-  if (options.additionalFields) {
-    attachment.fields = attachment.fields.concat(options.additionalFields);
-  }
-  if (options.additionalTags) {
-    tags = _.union(options.additionalTags, tags);
-  }
-  if (options.hideTags !== true && tags.length > 0) {
-    if (typeof tags === 'string') {
-      attachment.fields.push({ title: 'Tags', value: tags });
-    } else {
-      attachment.fields.push({ title: 'Tags', value: tags.join(', ') });
-    }
-  }
-  // set any colors for special tags:
-  if (tags.indexOf('success') > -1) {
-    attachment.color = 'good';
-  }
-  if (tags.indexOf('warning') > -1) {
-    attachment.color = 'warning';
-  }
-  if (tags.indexOf('error') > -1) {
-    attachment.color = 'danger';
-  }
-  // set any special channel:
-  const slackPayload = {
-    attachments: [attachment]
-  };
-  if (options.channel) {
-    slackPayload.channel = options.channel;
-  }
-  if (options.iconURL) {
-    slackPayload.icon_url = options.iconURL;
-  }
-  if (options.username) {
-    slackPayload.username = options.username;
-  }
-  return JSON.stringify(slackPayload);
-};
-
-// will format and doPost a server.log style message to slack:
-const postMessageToSlack = (tags, data) => {
-  let slackPayload;
-  // when called directly, tags is just an array:
-  if (_.isArray(tags)) {
-    slackPayload = makeSlackPayload(_.union(tags, tags).join(', '), data);
-  // when called as an event on server.log, tags will be an object
-  // // in which they keys are the tag names:
-  } else if (_.isObject(tags)) {
-    slackPayload = makeSlackPayload(_.union(_.keys(tags), tags).join(', '), data);
-  }
-  doPost(slackPayload);
-};
-
-// sends a payload string to slack:
-const doPost = (slackPayload) => {
-  if (_.isObject(slackPayload)) {
-    slackPayload = JSON.stringify(slackPayload);
-  }
-  Wreck.request('POST', options.slackHook, {
-    headers: { 'Content-type': 'application/json' },
-    payload: slackPayload
-  }, (err) => {
+  const settings = options || {};
+  const post2slack = new Post2Slack(settings);
+  post2slack.postFormatted(tags, message, (err) => {
     if (err) {
-      server.log(['hapi-slack'], err);
+      console.log(err);
     }
   });
-};
-
-module.exports.register = (passedOptions, callback) => {
-  options = passedOptions;
-  let methods = options.methods ? options.methods : undefined;
-  if (methods) {
-    methods.postMessageToSlack = postMessageToSlack;
-    methods.postRawDataToSlack = doPost;
-    methods.makeSlackPayload = makeSlackPayload;
-  }
-  callback();
-};
-
-module.exports.render = (options, tags, message) => {
-  postMessageToSlack(tags, message);
 };
--- test/slack.test.js
+++ test/slack.test.js
@@ -1,23 +1,15 @@
 'use strict';
 const expect = require('chai').expect;
-// this is a logr plugin, so it should be
-// installed as a package inside logr using
-// 'npm install logr-slack':
-const Logr = require('../../../index.js');
-const path = require('path');
+const Logr = require('logr');
 const Hapi = require('hapi');
+const logrSlack = require('../index.js');
 
-const logr_slack = require('../index.js');
 it('can load the slack plugin ', (done) => {
-  const server = new Hapi.Server({});
-  const log = new Logr({
+  const log = Logr.createLogger({
     type: 'slack',
-    plugins: {
-      slack: 'logr-slack'
-    },
-    renderOptions: {
+    reporters: {
       slack: {
-        methods: server.methods,
+        reporter: logrSlack,
         slackHook: process.env.SLACK_WEBHOOK,
         // you can provide a name for any channel allowed by the above slack webhook:
         channel: '#hapi-slack-test',
@@ -28,45 +20,15 @@ it('can load the slack plugin ', (done) => {
       }
     }
   });
-  expect(server.methods.postMessageToSlack).to.not.equal(undefined);
-  expect(server.methods.postRawDataToSlack).to.not.equal(undefined);
-  expect(server.methods.makeSlackPayload).to.not.equal(undefined);
-  done();
-});
-
-it('can use the exposed server methods to make a simple slack payload', (done) => {
-  const server = new Hapi.Server({});
-  const log = new Logr({
-    type: 'logr-slack',
-    plugins: {
-      'logr-slack': 'logr-slack'
-    },
-    renderOptions: {
-      'logr-slack': {
-        methods: server.methods,
-        slackHook: process.env.SLACK_WEBHOOK
-      }
-    }
-  });
-  const expectedPacket = {
-    attachments: [{
-      fallback: 'this is a posting',
-      title: 'this is a posting',
-      fields: [{
-        title: 'Tags',
-        value: 'tag1'
-      }]
-    }],
-  };
-  const payload = server.methods.makeSlackPayload(['tag1'], 'this is a posting');
-  expect(JSON.parse(payload)).to.deep.equal(expectedPacket);
+  expect(typeof log).to.equal('function');
   done();
 });
 
 it('can use logr to do a basic post to slack', (done) => {
   const server = new Hapi.Server({});
-  server.connection({port: 8080});
+  server.connection({ port: 8080 });
   const expectedPayload = {
+    channel: '#hapi-slack-test',
     attachments: [{
       fallback: 'a string',
       title: 'a string',
@@ -86,15 +48,20 @@ it('can use logr to do a basic post to slack', (done) => {
     }
   });
   server.start(() => {
-    const log = new Logr({
-      type: 'logr-slack',
-      plugins: {
-        'logr-slack': 'logr-slack'
-      },
-      renderOptions: {
-        'logr-slack': {
-          methods: server.methods,
-          slackHook: 'http://localhost:8080/',
+    const log = Logr.createLogger({
+      type: 'slack',
+      reporters: {
+        slack: {
+          reporter: logrSlack,
+          options: {
+            slackHook: 'http://localhost:8080',
+            // you can provide a name for any channel allowed by the above slack webhook:
+            channel: '#hapi-slack-test',
+            // you can list which tags will cause a server.log call to post to slack:
+            tags: ['warning', 'error', 'success', 'test'],
+            // you can specify tags that will automatically be appended to each post to slack:
+            additionalTags: ['server-test.js', 'someAdditionalTag']
+          }
         }
       }
     });
