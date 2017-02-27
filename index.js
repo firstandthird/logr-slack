17ecd9d1b1ccdd5512511049fbc0d17da1fac33a
Chris <orthagonal1@gmail.com>
2017-02-26 22:47:19 -0600

    tape tests

--- index.js
+++ index.js
@@ -2,13 +2,7 @@
 const Post2Slack = require('post2slack');
 
 module.exports.log = function(options, tags, message) {
-  if (!message) {
-    message = tags;
-    tags = options;
-    options = false;
-  }
-  const settings = options || {};
-  const post2slack = new Post2Slack(settings);
+  const post2slack = new Post2Slack(options);
   post2slack.postFormatted(tags, message, (err) => {
     if (err) {
       console.log(err);
--- test/slack.test.js
+++ test/slack.test.js
@@ -1,16 +1,17 @@
 'use strict';
-const expect = require('chai').expect;
 const Logr = require('logr');
 const Hapi = require('hapi');
 const logrSlack = require('../index.js');
+const test = require('tape');
 
-it('can load the slack plugin ', (done) => {
+test('can load the slack plugin ', (t) => {
+  t.plan(1);
   const log = Logr.createLogger({
     type: 'slack',
     reporters: {
       slack: {
         reporter: logrSlack,
-        slackHook: process.env.SLACK_WEBHOOK,
+        slackHook: 'no hook',
         // you can provide a name for any channel allowed by the above slack webhook:
         channel: '#hapi-slack-test',
         // you can list which tags will cause a server.log call to post to slack:
@@ -20,13 +21,10 @@ it('can load the slack plugin ', (done) => {
       }
     }
   });
-  expect(typeof log).to.equal('function');
-  done();
+  t.equal(typeof log, 'function', 'should register a "log" function');
 });
 
-it('can use logr to do a basic post to slack', (done) => {
-  const server = new Hapi.Server({});
-  server.connection({ port: 8080 });
+test('can use logr to do a basic post to slack', (t) => {
   const expectedPayload = {
     channel: '#hapi-slack-test',
     attachments: [{
@@ -38,13 +36,16 @@ it('can use logr to do a basic post to slack', (done) => {
       }]
     }],
   };
-
+  const server = new Hapi.Server({});
+  server.connection({ port: 8080 });
   server.route({
     method: 'POST',
     path: '/',
     handler: (request, reply) => {
-      expect(request.payload).to.deep.equal(expectedPayload);
-      done();
+      t.deepEqual(request.payload, expectedPayload, 'should return the payload in the appropriate format');
+      // setTimeout(() => {
+        server.stop(t.end);
+      // }, 1000);
     }
   });
   server.start(() => {
@@ -54,7 +55,7 @@ it('can use logr to do a basic post to slack', (done) => {
         slack: {
           reporter: logrSlack,
           options: {
-            slackHook: 'http://localhost:8080',
+            slackHook: 'http://localhost:8080/',
             // you can provide a name for any channel allowed by the above slack webhook:
             channel: '#hapi-slack-test',
             // you can list which tags will cause a server.log call to post to slack:
